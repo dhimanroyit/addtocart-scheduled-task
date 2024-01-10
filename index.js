3151a57feca20a04bfe5d83df969d92b82ef6249
dhimanroyit <dhimanroyit@gmail.com>
2024-01-11 00:19:41 +0600

    schedule notify when add to cart

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 import express from "express";
-import db from "./config/dbConnect.js";
-import cartModel from "./model/cartModel.js";
+import "dotenv/config.js";
+import db from "./src/config/dbConnect.js";
+import cartModel from "./src/model/cartModel.js";
+import { addToCartScheduleQueue } from "./src/messageQueue/addToCartScheduleQueue.js";
+import { addToCartScheduleWorker } from "./src/messageQueue/addToCartScheduleWorker.js";
 const app = express();
 
 app.use(express.json());
@@ -10,14 +13,51 @@ app.get("/", (req, res) => {
   res.send("welcome cart");
 });
 
-app.post("/carts", (req, res, next) => {});
+app.post("/carts", async (req, res, next) => {
+  const { user_id } = req.body;
+
+  await addToCartScheduleQueue.remove(user_id);
+
+  await addToCartScheduleQueue.add(
+    `${user_id}`,
+    {
+      data: req.body,
+    },
+    {
+      delay: 5000, // variable time it's upto you
+      jobId: `${user_id}`,
+    }
+  );
+
+  const cart = await cartModel.create(req.body);
+
+  res.json({
+    message: "cart add successfully",
+    data: cart,
+  });
+});
+
+addToCartScheduleWorker.run();
+
+app.use((err, req, res, next) => {
+  if (err) {
+    console.log(err);
+    res.json({
+      message: "error",
+      data: err,
+    });
+  }
+});
 
 app.listen(5000, () => {
   console.log("server running on port 5000");
   try {
     db.sequelize.authenticate();
-    console.log("db connected");
-  } catch (e) {
-    console.log("db error");
+    console.log("database connected successfully");
+    db.sequelize.sync({
+      // alter: true,
+    });
+  } catch (err) {
+    console.log("database error occur", err);
   }
 });
--- src/messageQueue/addToCartScheduleWorker.js
+++ src/messageQueue/addToCartScheduleWorker.js
@@ -3,6 +3,7 @@ import { Worker } from "bullmq";
 export const addToCartScheduleWorker = new Worker(
   "cart-schedule-notify",
   async (job) => {
+    // add any task in this function (like send email, push notification or message)
     console.log("worker", job.data);
   },
   {
